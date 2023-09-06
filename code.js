55791a601bdb2227c4ec03fd831c63ab1cc7ea93
Ferney Barbosa <barbosaferney05@gmail.com>
2023-09-06 05:59:07 -0500

    Haciendo cambios

--- code.js
+++ code.js
@@ -9,3 +9,13 @@ boton.addEventListener('click', () => {
     const estilosCSS = input.value;
     seccion.style.cssText = estilosCSS;
 })
+
+input.addEventListener('change', () => {
+    // Verifica si se seleccionó un archivo
+    if(input.files.length > 0) {
+      const file = input.files[0];
+      const imageURL = URL.createObjectURL(file);
+      seccion.style.backgroundImage = `url(${imageURL})`;
+      URL.revokeObjectURL(imageURL);
+    }
+});
